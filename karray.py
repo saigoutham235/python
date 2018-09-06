0d3011fdf7ce9c044eefcd38a833e1a0e5e58837
saigoutham235 <41940449+saigoutham235@users.noreply.github.com>
2018-09-06 16:02:45 +0530

    Update karray.py

--- karray.py
+++ karray.py
@@ -1,7 +1,7 @@
-n,k=map(int,raw_input().split())
-n=list(map(int,raw_input().split()))
-sum=0
-for i in range(0,(k+1)):
-	sum=sum+i
-	n.append(sum)
-print(sum)
+b,s=map(int,raw_input().split())
+b=list(map(int,raw_input().split()))
+s=0
+for i in range(0,s+1)):
+	s=s+i
+	n.append(s)
+print(s)
