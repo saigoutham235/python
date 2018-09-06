2740546665b8bd92d5a5fc3ceeeabf56d979cd55
saigoutham235 <41940449+saigoutham235@users.noreply.github.com>
2018-09-06 16:03:09 +0530

    Update karray.py

--- karray.py
+++ karray.py
@@ -1,7 +1,7 @@
-b,s=map(int,raw_input().split())
+b,g=map(int,raw_input().split())
 b=list(map(int,raw_input().split()))
 s=0
-for i in range(0,s+1)):
+for i in range(0,g+1)):
 	s=s+i
 	n.append(s)
 print(s)
