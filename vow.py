dcbac128655718963dc08144b281fe4f18710535
saigoutham235 <41940449+saigoutham235@users.noreply.github.com>
2018-09-07 15:09:02 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,8 +1,10 @@
 ch=list(input())
+count=0
 for i in ch:
-  if((i>'a' and i<'z') or (i>'A' and i<'Z')):
-    print("yes")
-    break
-  else:
-    print("no")
-    break
+  if(i=='a' or i=='e' or i=='i' or i=='u' or i=='o' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+   count=count+1
+   break
+if(count==0):
+  print("no")
+else:
+  print("yes")
