3e166e6d5dbfc14f9983af50a82f649121940439
saigoutham235 <41940449+saigoutham235@users.noreply.github.com>
2021-01-10 13:35:39 +0530

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,35 @@
+M = 3
+N = 3
+
+def printf(mat):
+    
+	global M,N
+    
+		for i in range(M):
+        
+			if i%2 == 0:
+            
+				for j in range(N):
+                
+			                	print(str(mat[i][j]),
+			  
+                          	end = " ")
+        
+			else:
+            
+				for j in range(N -1,-1,-1):
+                 
+				 	print(str(mat[i][j]),  
+                          
+					end = " ")
+
+mat = [[ 10, 20, 30 ], 
+      
+           [ 15, 25, 35 ], 
+      
+           [ 27, 29, 37 ]] 
+
+       
+
+printf(mat)
+  
